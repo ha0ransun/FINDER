8740a3f81d3fe7a0eef282d8e665874aef64b802
riemannnn@gmail.com <riemannnn@gmail.com>
2020-10-16 11:14:29 -0400

    output results

--- code/FINDER_CN/testSynthetic.py
+++ code/FINDER_CN/testSynthetic.py
@@ -15,7 +15,7 @@ def main():
         for cost in cost_types:
             data_test_path = '../../data/synthetic/{}/{}/'.format(graph_type, cost)
             data_test_name = ['30-50', '50-100', '100-200'] #, '200-300', '300-400', '400-500']
-            model_file = f'./models/Model_{graph_type}/nrange_30_50_iter_9900_{graph_type}_{cost}.ckpt'
+            model_file = f'./models/Model_{graph_type}/nrange_30_50_iter_9900_{graph_type}.ckpt'
 
             file_path = '../../results/FINDER_CN/synthetic'
             if not os.path.exists('../../results'):
@@ -31,6 +31,7 @@ def main():
                     score_mean, score_std, time_mean, time_std = dqn.Evaluate(data_test, model_file)
                     fout.write('%.2f±%.2f,' % (score_mean * 100, score_std * 100))
                     fout.flush()
+                    print('score mean: {}, score std: {}'.format(score_mean * 100, score_std * 100))
                     print('data_test_%s has been tested!'.format(data_test_name[i]))
 
 
