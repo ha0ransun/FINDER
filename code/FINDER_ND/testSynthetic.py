c4ceeec49d84f4df20b4d8192b3df628b7f25dc7
riemannnn@gmail.com <riemannnn@gmail.com>
2020-11-30 00:46:42 -0500

    update evaluation to larger size

--- code/FINDER_ND/testSynthetic.py
+++ code/FINDER_ND/testSynthetic.py
@@ -11,31 +11,24 @@ def main():
     dqn = FINDER()
     graph_types = ['barabasi_albert', 'erdos_renyi', 'small-world']
     cost_types = ['uniform']
+    file_path = '../../results/FINDER_ND/synthetic'
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
     for graph_type in graph_types:
-        for cost in cost_types:
-            for problem_type in graph_types:
-                data_test_path = '../../data/synthetic/{}/{}/'.format(problem_type, cost)
-                data_test_name = ['30-50', '50-100', '100-200'] #, '200-300', '300-400', '400-500']
-                model_file = f'./models/Model_{graph_type}/nrange_30_50_iter_99900_{graph_type}.ckpt'
+        for cost_type in cost_types:
+            data_test_path = '../../data/synthetic/{}/{}/'.format(graph_type, cost_type)
+            data_test_name = ['30-50', '50-100', '100-200', '200-300', '300-400', '400-500']
+            model_file = f'./models/Model_{graph_type}/nrange_30_50_iter_37500_{graph_type}_{cost_type}.ckpt'
 
-                file_path = '../../results/FINDER_CN/synthetic'
-                if not os.path.exists('../../results'):
-                    os.mkdir('../../results')
-                if not os.path.exists('../../results/FINDER_ND'):
-                    os.mkdir('../../results/FINDER_ND')
-                if not os.path.exists('../../results/FINDER_ND/synthetic'):
-                    os.mkdir('../../results/FINDER_ND/synthetic')
-
-                with open('%s/%s_%s_score.txt'%(file_path, problem_type, cost), 'w') as fout:
-                    for i in tqdm(range(len(data_test_name))):
-                        data_test = data_test_path + data_test_name[i]
-                        score_mean, score_std, time_mean, time_std = dqn.Evaluate(data_test, model_file)
-                        # fout.write('%.2f±%.2f,' % (score_mean * 100, score_std * 100))
-                        # fout.flush()
-                        print(100*'#')
-                        print('method graph: {}, problem graph: {}'.format(graph_type, problem_type))
-                        print('score mean: {}, score std: {}'.format(score_mean * 100, score_std * 100))
-                        print('data_test_{} has been tested!'.format(data_test_name[i]))
+            with open('%s/%s_%s_score.txt' % (file_path, graph_type, cost_type), 'w') as fout:
+                for i in tqdm(range(len(data_test_name))):
+                    data_test = data_test_path + data_test_name[i]
+                    score_mean, score_std, time_mean, time_std = dqn.Evaluate(data_test, model_file)
+                    fout.write('%.2f±%.2f,' % (score_mean * 100, score_std * 100))
+                    fout.flush()
+                    print(100 * '#')
+                    print('data_test_{} has been tested!'.format(data_test_name[i]))
 
 
 if __name__=="__main__":
