3ef4b0f50fa774a01c157187d56ddc4f5f29cf10
michaely1028 <33078035+michaely1028@users.noreply.github.com>
2020-12-02 03:17:55 -0500

    Update for testing real graph

--- code/FINDER_CN/testReal.py
+++ code/FINDER_CN/testReal.py
@@ -13,7 +13,7 @@ def GetSolution(STEPRATIO, MODEL_FILE_CKPT):
     dqn = FINDER()
     data_test_path = '../data/real/'
 #     data_test_name = ['Crime','HI-II-14','Digg','Enron','Gnutella31','Epinions','Facebook','Youtube','Flickr']
-    data_test_name = ['Crime','HI-II-14']
+    data_test_name = ['Crime','HI-II-14', 'US_airports_unweighted']
     model_file_path = './FINDER_CN/models/'
     model_file_ckpt = MODEL_FILE_CKPT
     model_file = model_file_path + model_file_ckpt
@@ -45,7 +45,7 @@ def EvaluateSolution(STEPRATIO, MODEL_FILE_CKPT, STRTEGYID):
     dqn = FINDER()
     data_test_path = '../data/real/'
 #     data_test_name = ['Crime', 'HI-II-14', 'Digg', 'Enron', 'Gnutella31', 'Epinions', 'Facebook', 'Youtube', 'Flickr']
-    data_test_name = ['Crime','HI-II-14']
+    data_test_name = ['Crime','HI-II-14', 'US_airports_unweighted']
     save_dir = '../results/FINDER_CN/real/StepRatio_%.4f/'%STEPRATIO
     ## begin computing...
     df = pd.DataFrame(np.arange(2 * len(data_test_name)).reshape((2, len(data_test_name))), index=['solution', 'time'], columns=data_test_name)
@@ -70,9 +70,9 @@ def EvaluateSolution(STEPRATIO, MODEL_FILE_CKPT, STRTEGYID):
 
 
 def main():
-    model_file_ckpt = 'nrange_30_50_iter_93300.ckpt'
+    model_file_ckpt = 'nrange_30_50_iter_400000_barabasi_albert.ckpt'
     GetSolution(0.01, model_file_ckpt)
-    EvaluateSolution(0.01, model_file_ckpt, 0)
+    # EvaluateSolution(0.01, model_file_ckpt, 0)
 
 
 if __name__=="__main__":
